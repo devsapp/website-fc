f72c88df9430784c31f8207225239b78ad27378a
xsahxl <xsahxl@126.com>
2022-03-27 15:06:24 +0800

    feat: 🎸 dev code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import { loadComponent } from "@serverless-devs/core";
-
+const core = require("@serverless-devs/core");
+const path = require("path");
+const { lodash, fse, rimraf } = core;
 /**
  * Plugin 插件入口
  * @param inputs 组件的入口参数
  * @param args 插件的自定义参数
  * @return inputs
  */
-export default async function (inputs, args) {
-  const fc = await loadComponent("fc");
-  await fc.deploy(inputs);
-  return { ...inputs };
-}
+
+module.exports = async function index(inputs, args) {
+  const codeUri = lodash.get(inputs, "props.function.codeUri");
+  if (lodash.isEmpty(codeUri)) return;
+  const bashPath = path.dirname(lodash.get(inputs, "path.configPath"));
+  const newCodeUri = path.isAbsolute(codeUri)
+    ? codeUri
+    : path.join(bashPath, codeUri);
+  const publicPath = path.join(__dirname, "./server/public");
+  fse.ensureDirSync(publicPath);
+  rimraf.sync(publicPath);
+  fse.copySync(newCodeUri, publicPath);
+  return lodash.merge(inputs, {
+    props: {
+      function: {
+        codeUri: path.join(__dirname, "./server"),
+        customRuntimeConfig: {
+          command: ["node"],
+          args: ["/server/index.js"],
+        },
+      },
+    },
+  });
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,7 @@
-const serverless = require("@serverless-devs/fc-http");
-const express = require("express");
+const serve = require("koa-static");
+const Koa = require("koa");
+const app = new Koa();
 const path = require("path");
 
-const startServer = (args) => {
-  const app = express();
-
-  app.use(express.static(args.staticPath || path.join(__dirname, "public")));
-
-  app.get("/user", (req, res) => {
-    res.send("hello user!");
-  });
-  return serverless(app);
-};
-
-export default startServer;
+app.use(serve(path.join(__dirname, "./public")));
+app.listen(9000);
