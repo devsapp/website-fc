952ffd4af48f39f1bf6638fbe764600f9a354ade
DevDengChao <2325690622@qq.com>
2026-02-10 14:15:41 +0800

    fix: logger not present
    Signed-off-by: DevDengChao <2325690622@qq.com>

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ const rimraf = require("rimraf");
  */
 
 module.exports = async function index(inputs, args, logger) {
-  logger.debug(`inputs params: ${JSON.stringify(inputs)}`);
-  logger.debug(`args params: ${JSON.stringify(args)}`);
+  logger?.debug(`inputs params: ${JSON.stringify(inputs)}`);
+  logger?.debug(`args params: ${JSON.stringify(args)}`);
   const codeUri = lodash.get(inputs, "props.code");
   if (lodash.isEmpty(codeUri)) return;
   const bashPath = lodash.get(inputs, "cwd");
